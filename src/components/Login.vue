a62d33a49254f2aca9948f413fc18c299be6eaa1
Jhoni Faber <jhonatanfaber@gmail.com>
2018-11-28 17:49:32 +0000

    remove unnecessary css variables

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -102,8 +102,6 @@ html {
 .btn {
   font-weight: 700;
   height: 36px;
-  -moz-user-select: none;
-  -webkit-user-select: none;
   user-select: none;
   cursor: default;
 }
@@ -113,11 +111,7 @@ html {
   padding: 20px 25px 30px;
   margin: 0 auto 25px;
   margin-top: 50px;
-  -moz-border-radius: 2px;
-  -webkit-border-radius: 2px;
   border-radius: 2px;
-  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
 }
 
@@ -126,8 +120,6 @@ html {
   height: 96px;
   margin: 0 auto 10px;
   display: block;
-  -moz-border-radius: 50%;
-  -webkit-border-radius: 50%;
   border-radius: 50%;
 }
 
@@ -149,8 +141,6 @@ html {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
   box-sizing: border-box;
 }
 
@@ -168,50 +158,35 @@ html {
   width: 100%;
   display: block;
   margin-bottom: 10px;
-  z-index: 1;
-  position: relative;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
 }
 
 .form-signin .form-control:focus {
   border-color: rgb(104, 145, 162);
-  outline: 0;
-  -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
-    0 0 8px rgb(104, 145, 162);
   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgb(104, 145, 162);
 }
 
 .btn.btn-signin {
-  background-color: rgb(104, 145, 162);
-  padding: 0px;
+  background-color: #343a40;
   font-weight: 700;
   font-size: 14px;
   height: 36px;
-  -moz-border-radius: 3px;
-  -webkit-border-radius: 3px;
-  border-radius: 3px;
+  border-radius: 4px;
   border: none;
-  -o-transition: all 0.218s;
-  -moz-transition: all 0.218s;
-  -webkit-transition: all 0.218s;
-  transition: all 0.218s;
 }
 
 .btn.btn-signin:hover,
 .btn.btn-signin:active,
 .btn.btn-signin:focus {
-  background-color: rgb(117, 160, 179);
+  background-color: rgb(0, 0, 0);
 }
 
 .forgot-password {
-  color: rgb(104, 145, 162);
+  color: #343a40
 }
 
 .forgot-password:hover,
 .forgot-password:active,
 .forgot-password:focus {
-  color: rgb(12, 97, 33);
+  color: rgb(0, 0, 0);
 }
 </style>
