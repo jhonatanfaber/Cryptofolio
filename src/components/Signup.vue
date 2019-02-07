5fd0aa5c68a62e349c72a7bbfc90acec6f6fe6db
Jhoni Faber <jhonatanfaber@gmail.com>
2019-02-07 10:59:46 +0000

    fix sign uo ui

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,85 +1,107 @@
 <template>
   <div class="signup-container">
     <div class="container">
-      <div class="login-container">
-        <h4>Create New Account</h4>
-        <form class="form-signin">
-          <div class="input-form">
-            <input
-              type="text"
-              v-model="name"
-              @input="$v.name.$touch()"
-              id="exampleInputName"
-              class="form-control form-group col-sm-3"
-              placeholder="Name"
-              :class="{invalidName : $v.name.$error}"
-            >
-            <span class="error" v-if="$v.name.$error">Field is required</span>
-          </div>
-          <div class="input-form">
-            <input
-              type="text"
-              v-model="username"
-              @input="$v.username.$touch()"
-              id="exampleInputUsername"
-              class="form-control form-group col-sm-3"
-              placeholder="Username"
-              :class="{invalidUsername : $v.username.$error, 'signup-error': (signup_status == 'error')}"
-            >
-            <div class="error" v-if="$v.username.$error">Field is required</div>
-          </div>
-          <div class="input-form">
-            <input
-              type="text"
-              v-model="email"
-              @input="$v.email.$touch()"
-              id="exampleInputEmail"
-              class="form-control form-group col-sm-3"
-              placeholder="Email"
-              :class="{invalidEmail : $v.email.$error, 'signup-error': (signup_status == 'error')}"
-            >
-            <div
-              class="error"
-              v-if="$v.email.$error"
-            >Field is required and must be a valid email address</div>
-          </div>
-          <div class="input-form">
-            <input
-              type="password"
-              v-model="password"
-              @input="$v.password.$touch()"
-              id="exampleInputPassword"
-              class="form-control form-group col-sm-3"
-              placeholder="Password"
-              :class="{invalidPassword : $v.password.$error}"
-            >
-            <div class="error" v-if="$v.password.$error">Field is required</div>
-          </div>
-          <div class="input-form">
-            <input
-              type="password"
-              v-model="repeatedPassword"
-              @input="$v.repeatedPassword.$touch()"
-              id="exampleInputRepeatedPassword"
-              class="form-control form-group col-sm-3"
-              placeholder="Repeat password"
-              :class="{invalidRepeatedPassword : $v.repeatedPassword.$error}"
-            >
-            <div
-              class="error"
-              v-if="$v.repeatedPassword.$error"
-            >Field is required and must be identical</div>
-            <div
-              class="already-exists"
-              v-if="signup_status == 'error'"
-            >Username or email already exist</div>
-          </div>
-          <button
-            @click.prevent="signup"
-            :disabled="!$v.name.required || !$v.password.required || !$v.username.required ||  !$v.repeatedPassword.sameAsPassword || $v.email.$invalid"
-            class="btn btn-lg btn-primary btn-signin col-sm-3"
-          >Sign up</button>
-        </form>
+      <h2>Create new Account</h2>
+      <hr>
+      <div class="row">
+        <div class="col-md-9 personal-info">
+          <form class="form-horizontal" role="form">
+            <div class="form-group">
+              <label class="col-lg-3 control-label">Name:</label>
+              <div class="col-md-8">
+                <input
+                  type="text"
+                  v-model="name"
+                  @input="$v.name.$touch()"
+                  id="exampleInputName"
+                  class="form-control"
+                  placeholder="Name"
+                  :class="{invalidName : $v.name.$error}"
+                >
+              </div>
+              <div class="error" v-if="$v.name.$error">Field is required</div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-3 control-label">Password:</label>
+              <div class="col-md-8">
+                <input
+                  type="text"
+                  v-model="username"
+                  @input="$v.username.$touch()"
+                  id="exampleInputUsername"
+                  class="form-control"
+                  placeholder="Username"
+                  :class="{invalidUsername : $v.username.$error, 'signup-error': (signup_status == 'error')}"
+                >
+              </div>
+              <div class="error" v-if="$v.username.$error">Field is required</div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-3 control-label">Email:</label>
+              <div class="col-md-8">
+                <input
+                  type="text"
+                  v-model="email"
+                  @input="$v.email.$touch()"
+                  id="exampleInputEmail"
+                  class="form-control"
+                  placeholder="Email"
+                  :class="{invalidEmail : $v.email.$error, 'signup-error': (signup_status == 'error')}"
+                >
+              </div>
+              <div
+                class="error"
+                v-if="$v.email.$error"
+              >Field is required and must be a valid email address</div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-3 control-label">Password:</label>
+              <div class="col-md-8">
+                <input
+                  type="password"
+                  v-model="password"
+                  @input="$v.password.$touch()"
+                  id="exampleInputPassword"
+                  class="form-control"
+                  placeholder="Password"
+                  :class="{invalidPassword : $v.password.$error}"
+                >
+              </div>
+              <div class="error" v-if="$v.password.$error">Field is required</div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-3 control-label">Confirm password:</label>
+              <div class="col-md-8">
+                <input
+                  type="password"
+                  v-model="repeatedPassword"
+                  @input="$v.repeatedPassword.$touch()"
+                  id="exampleInputRepeatedPassword"
+                  class="form-control"
+                  placeholder="Repeat password"
+                  :class="{invalidRepeatedPassword : $v.repeatedPassword.$error}"
+                >
+              </div>
+              <div
+                class="error"
+                v-if="$v.repeatedPassword.$error"
+              >Field is required and must be identical</div>
+              <div
+                class="already-exists"
+                v-if="signup_status == 'error'"
+              >Username or email already exist</div>
+            </div>
+            <div class="form-group buttons">
+              <div class="col-md-8">
+                <button
+                  @click.prevent="signup"
+                  :disabled="!$v.name.required || !$v.password.required || !$v.username.required ||  !$v.repeatedPassword.sameAsPassword || $v.email.$invalid"
+                  class="btn btn-lg btn-dark btn-signin col-sm-4"
+                >Sign up</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -141,53 +163,43 @@ export default {
 
 <style scoped>
 .container {
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  margin-top: 50px;
 }
 
-/* .signup-container {
+@media only screen and (min-width: 320px) and (max-width: 480px) {
+  .container {
+    margin-top: 20px;
+  }
+
+  h2{
+    font-size: 1.15rem;
+  }
+
+  hr{
+    display: none;
+  }
+}
+
+.form-group {
   display: flex;
-  flex-wrap: wrap;
-} */
-
-/* Mobile Styles */
-@media (max-width: 400px) {
-  .container {
-    top: 55%;
-  }
+  flex-direction: column;
+  align-items: flex-start;
 }
 
-/* Tablet Styles */
-@media only screen and (min-width: 401px) and (max-width: 960px) {
-  .container {
-    top: 45%;
-  }
+.form-group.buttons {
+  margin-top: 20px;
 }
 
-/* Desktop Styles */
-@media only screen and (min-width: 961px) {
-  .container {
-    top: 40%;
-  }
-
-  /* .signup-container {
-    width: 960px;
-    margin: 0 auto;
-  } */
-}
-
-.input-form {
-  width: 100%;
+.col-lg-3,
+.col-md-3 {
   display: flex;
-  justify-content: center;
 }
 
-.btn {
-  font-weight: 700;
-  height: 36px;
-  user-select: none;
-  cursor: default;
+.row {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  margin-top: 20px;
 }
 
 input:hover,
@@ -205,12 +217,6 @@ input:focus {
 .form-control:focus {
   border-color: #525151;
   box-shadow: inset 0px 0px 1px rgba(126, 124, 124, 0.87), 0 0 8px #7c7b7b;
-}
-
-form.form-signin {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 }
 
 input:hover,
@@ -241,7 +247,7 @@ input:focus {
   color: #f57f6c;
   font-size: 0.75rem;
   line-height: 2;
-  margin-left: 10px;
+  margin-left: 20px;
   display: flex;
 }
 
@@ -256,13 +262,6 @@ input:focus {
 
 button:disabled {
   cursor: not-allowed;
-}
-
-.form-signin #exampleInputEmail1,
-.form-signin #exampleInputPassword {
-  direction: ltr;
-  height: 38px;
-  font-size: 13px;
 }
 
 .btn.btn-signin {
