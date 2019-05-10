e57999e15a967b19e00d88adbd0209c8e5711e6b
Jhoni Faber <jhonatanfaber@gmail.com>
2019-05-10 13:54:07 +0100

    ignore

--- src/customisedAxios/customisedAxios.js
+++ src/customisedAxios/customisedAxios.js
@@ -1,8 +1,7 @@
 import axios from "axios"
 
-// https://api.cryptofolio.cf
 export const BASE_URL = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://api.cryptofolio.cf'
 });
 
 
